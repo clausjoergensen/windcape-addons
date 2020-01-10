a60f4e528e4b23e7c4cfceda570163a924b3023c
Claus Jørgensen <cjorgensen@zendesk.com>
2020-01-10 18:54:34 +0000

    reverted to a working state

--- Modules/Chat.lua
+++ Modules/Chat.lua
@@ -3,32 +3,9 @@
 local showingTooltip = false
 
 function Windcape:Chat_OnEnable()
-	-- self:SecureHook("ChatFrame_MessageEventHandler")
-
-	local filter = function(frame, event, message, ...)
-		local frameName = frame:GetName()
-		self.db.char.scrollback[frameName] = self.db.char.scrollback[frameName] or {}
-		
-		local scrollback = self.db.char.scrollback[frameName]
-		table.insert(scrollback, { message, ... })
-
-		self.db.char.scrollbackTime = time()
-		if #scrollback > self.db.char.scrollbackLength then
-			table.remove(scrollback, 1)
-		end
-
-		-- To leave the message untouched, return false or nil.
-		-- You don't need to explicitly do this, so the
-		-- "else return false" in this example is not required.
-		return false
-	end
-
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filter)
-
 	self:Chat_UpdateLayout()
 	self:Chat_EnableClassColors()
 	self:Chat_EnableHoverTips()
-	self:Chat_RestoreLastSession()
 end
 
 function Windcape:Chat_OnDisable()
@@ -102,42 +79,3 @@ function Windcape:Chat_EnableHoverTips()
 		end)
 	end
 end
-
-function Windcape:Chat_RestoreLastSession()
-	local textAdded = false
-	print(pairs(self.db.char.scrollback))
-	for key, history in pairs(self.db.char.scrollback) do
-		local frame = _G[key]
-		print(frame)
-		if frame then
-			for _, line in ipairs(history) do
-				frame:AddMessage(unpack(line))
-				textAdded = true
-			end
-
-			if textAdded then
-				frame:AddMessage(format(TIME_DAYHOURMINUTESECOND,
-								 ChatFrame_TimeBreakDown(time() - self.db.char.scrollbackTime)))
-			end
-		end
-	end
-end
-
--- function Windcape:ChatFrame_OnChatMessage(info, message, frame, event, text, r, g, b, id, ...)
--- 	local frameName = frame:GetName()
--- 	self.scrollback[frameName] = self.scrollback[frameName] or {}
-	
--- 	local scrollback = self.scrollback[frameName]
--- 	table.insert(scrollback, { text, r, g, b, id, ... })
-
--- 	self.db.char.scrollbackTime = time()
--- 	if #scrollback > self.db.profile.scrollbackLength then
--- 		table.remove(scrollback, 1)
--- 	end
--- end
-
--- function Windcape:ChatFrame_MessageEventHandler(event, ...)
--- 	if (strsub(event, 1, 8) == "CHAT_MSG") then
--- 		self:ChatFrame_OnChatMessage(...)
--- 	end
--- end
