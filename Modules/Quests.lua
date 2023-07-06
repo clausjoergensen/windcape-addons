5d7d845103b7f5abd38658873607ccd831f5773a
Claus Jørgensen <claus.joergensen@outlook.com>
2023-07-06 19:44:35 +0200

    Update Quests.lua

--- Modules/Quests.lua
+++ Modules/Quests.lua
@@ -12,12 +12,13 @@ end
 
 function Windcape_Quests:PLAYER_ENTERING_WORLD() 
     hooksecurefunc(WatchFrame, "SetPoint", function(self)
-        if not setting then
-            setting = true
-            self:ClearAllPoints()
-            self:SetPoint("TOPLEFT", "UIParent", "TOPLEFT", 20, -20)
-            self:SetHeight(600)
-            setting = nil
+        if setting then
+            return
         end
+        setting = true
+        self:ClearAllPoints()
+        self:SetPoint("TOPLEFT", "UIParent", "TOPLEFT", 20, -20)
+        self:SetHeight(600)
+        setting = nil
     end)
 end
