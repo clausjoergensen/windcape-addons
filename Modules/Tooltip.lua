ebaa5a108a12514cb2372790c255241ba7fe6cc6
Claus Jørgensen <claus.joergensen@outlook.com>
2023-07-13 08:33:51 +0200

    Update Tooltip.lua

--- Modules/Tooltip.lua
+++ Modules/Tooltip.lua
@@ -3,28 +3,31 @@
 Windcape_Tooltip = Windcape:NewModule("Tooltip", "AceEvent-3.0", "AceHook-3.0")
 
 function Windcape_Tooltip:OnEnable()
-    GameTooltip:HookScript("OnTooltipSetUnit", function(tooltip)
-        GameTooltipStatusBar:Hide() -- Hide health bar
-        
-        tooltip:ClearAllPoints()
-        tooltip:SetPoint("BOTTOMRIGHT", WorldFrame, "BOTTOMRIGHT", -10, 10)
-    end)
-
-    GameTooltip:HookScript("OnTooltipSetItem", function(tooltip, ...)
-        local item = tooltip:GetItem()
-        if not item then
-            return
-        end
-
-        local _, _, _, itemLevel = GetItemInfo(item)        
-        if itemLevel then
-            tooltip:AddLine("Item Level "..itemLevel) 
-        end
-
-        tooltip:Show()
-    end)
+    self:HookScript(GameTooltip, "OnTooltipSetUnit")
+    self:HookScript(GameTooltip, "OnTooltipSetItem")
 end
 
 function Windcape_Tooltip:OnDisable()
     GameTooltip:Unhook("OnTooltipSetItem")
+end
+
+function Windcape_Tooltip:OnTooltipSetUnit(tooltip)
+    GameTooltipStatusBar:Hide() -- Hide health bar
+        
+    tooltip:ClearAllPoints()
+    tooltip:SetPoint("BOTTOMRIGHT", WorldFrame, "BOTTOMRIGHT", -10, 10)
+end
+
+function Windcape_Tooltip:OnTooltipSetItem(tooltip)
+    local item = tooltip:GetItem()
+    if not item then
+        return
+    end
+
+    local _, _, _, itemLevel = GetItemInfo(item)        
+    if itemLevel then
+        tooltip:AddLine("Item Level " .. itemLevel) 
+    end
+
+    tooltip:Show()
 end
